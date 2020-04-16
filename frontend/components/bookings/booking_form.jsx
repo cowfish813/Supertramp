e3d9fab5f633e8eb59867d3246e0faa6be32b195
Nick_Cheung <nkkcheung@gmail.com>
2020-04-16 14:13:58 -0700

    booking

--- frontend/components/bookings/booking_form.jsx
+++ frontend/components/bookings/booking_form.jsx
@@ -14,7 +14,9 @@ class BookingForm extends React.Component {
             listingName: this.props.listing_name,//INVESTIGATE STATE OF LISTING.
             check_in: null,
             check_out: null,
-            capacity: 1
+            capacity: 1,
+            focusedStart: null,
+            focusedEnd: null,
         }
         this.highlighted = this.highlighted.bind(this);
         this.handleCapacity = this.handleCapacity.bind(this);
@@ -53,41 +55,64 @@ class BookingForm extends React.Component {
 
 
     render () {
-        {/* https://github.com/airbnb/react-dates */}
-        {/* MUST go through this documentation beore writing the rest of it */}
-
         return (
-
-                <div className="widget-container">
-                    <form className="wrapper" onSubmit={this.handleSubmit}>
-                        <div className="price-wrapper">
-                            <div className="price">{this.props.list.price}</div>
-                        </div>
-                        <div className="dates-and-guest-content">
-                            <div className="col checkin">
-                                <div className="label">
-                                    Check in
-                    <input type="date" className="col_box" />
-                                </div>
-                            </div>
-                            <div className="col checkout">
-                                <div className="label">
-                                    Check out
-                    <input type="date" name="" id="" className="col_box" />
-                                </div>
-                            </div>
-                            <div className="col capacity">
-                                <div className="label">
-                                    Guests
-                    <input type="number" name="" id="" className="col_box" />
-                                </div>
-                            </div>
-                        </div>
-                        <button className="booking-button">Book Now</button>
-                    </form>
+          <div className="widget-container">
+            <form className="wrapper" onSubmit={this.handleSubmit}>
+              <div className="price-wrapper">
+                <div className="price">{this.props.list.price}</div>
+              </div>
+              <div className="dates-and-guest-content">
+                <div className="col checkin">
+                  <div className="label">
+                    Check in
+                    <SingleDatePicker
+                      placeholder="Check In"
+                      date={this.state.check_in} // momentPropTypes.momentObj or null
+                      onDateChange={(date) => this.setState({ date })} // PropTypes.func.isRequired
+                      focused={this.state.focused} // PropTypes.bool
+                      onFocusChange={({ focused }) =>
+                        this.setState({ focusedStart: focused })
+                      } // PropTypes.func.isRequired
+                      className="col_box" // PropTypes.string.isRequired,
+                      verticalSpacing={0}
+                      isDayHighlighted={(day) => this.highlighted(day)} //method written
+                      numberOfMonths={1}
+                      daySize={36}
+                      noBorder={true}
+                    />
+                    {/* <input type="date" className="col_box" /> */}
+                  </div>
                 </div>
-
-        )
+                <div className="col checkout">
+                  <div className="label">
+                    Check out
+                    <SingleDatePicker
+                      placeholder="Check In"
+                      date={this.state.check_out} // momentPropTypes.momentObj or null
+                      onDateChange={(date) => this.setState({ date })} // PropTypes.func.isRequired
+                      focused={this.state.focused} // PropTypes.bool
+                      onFocusChange={({ focused }) => this.setState({ focusedEnd: focused })} // PropTypes.func.isRequired
+                      className="col_box" // PropTypes.string.isRequired,
+                      verticalSpacing={0}
+                      isDayHighlighted={(day) => this.highlighted(day)} //method written
+                      numberOfMonths={1}
+                      daySize={36}
+                      noBorder={true}
+                    />
+                    {/* <input type="date" name="" id="" className="col_box" /> */}
+                  </div>
+                </div>
+                <div className="col capacity">
+                  <div className="label">
+                    Guests
+                    <input type="number" name="" id="" className="col_box" />
+                  </div>
+                </div>
+              </div>
+              <button className="booking-button">Book Now</button>
+            </form>
+          </div>
+        );
     }
 
 }
