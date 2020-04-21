a969d377cdc7b7b366840e1c26657390b2d31109
Nick_Cheung <nkkcheung@gmail.com>
2020-04-21 15:34:50 -0700

    working maps

--- frontend/components/listing/list_map.jsx
+++ frontend/components/listing/list_map.jsx
@@ -1,35 +1,65 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-// import MarkerManager from '../../util/map_marker'
 
 class ListMap extends React.Component {
     constructor(props) {
         super(props)
+    };
 
-    }
-    componentDidMount() {
-        window.scrollTo(0, 0);
-        
-        this.mapNode = document.getElementById('mapNode');
+    // componentDidMount() {
+    //     window.scrollTo(0, 0);
+    
+    //     const mapOptions = {
+    //         // center: { lat: 37.7758, lng: -122.435 }, 
+    //       center: { 
+    //           lat: this.props.list.lat, 
+    //           lng: this.props.list.lng 
+    //         },
+    //       zoom: 13,
+    //       mapTypeId: "terrain", //sets map to terrain type
+    //     };
+
+    //     // const mapRef = this.refs.map;
+    //     this.mapNode = document.getElementById("mapNode")
+    //     console.log("map node", this.mapNode)
+    //     this.map = new google.maps.Map(this.mapNode, mapOptions);
+    //     let center = {
+    //         lat: this.props.list.lat,
+    //         lng: this.props.list.lng,
+    //     };
+
+    //     const circle = new google.maps.Circle({
+    //         strokeColor: '51D9AC',
+    //         strokeOpacity: 0.8,
+    //         strokeWeight: 2,
+    //         fillColor: "#71DBB4",
+    //         fillOpacity: 0.35,
+    //         map: this.map,
+    //         center: center,
+    //         radius: 1800
+    //     });
+    // };
+
+    componentDidUpdate() {
+        console.log("try")
+        // if (!this.props.list.lng) {
+        //     this.props.fetchListing(this.props.listId)
+        // }
 
         const mapOptions = {
             // center: { lat: 37.7758, lng: -122.435 }, 
-          center: { 
-              lat: +this.props.list.lat, 
-              lng: +this.props.list.lng 
+            center: {
+                lat: this.props.list.lat,
+                lng: this.props.list.lng
             },
-          zoom: 13,
-          mapTypeId: "terrain", //sets map to terrain type
+            zoom: 13,
+            mapTypeId: "terrain", //sets map to terrain type
         };
-
-        // const mapRef = this.refs.map;
-        // console.log("map node", this.mapNode)
         this.map = new google.maps.Map(this.mapNode, mapOptions);
         let center = {
             lat: this.props.list.lat,
             lng: this.props.list.lng,
         };
-
 
         const circle = new google.maps.Circle({
             strokeColor: '51D9AC',
@@ -41,15 +71,11 @@ class ListMap extends React.Component {
             center: center,
             radius: 1800
         });
-    };
+    }
     
 
     render () {
-        if (this.props.list.lng) {
-            return (<div className="map" id="mapNode" ></div>);
-        } else {
-            return <div className="map" id="mapNode" ref={(map) => (this.mapNode = map)}></div>
-        };
+            return (<div className="map" id="mapNode" ref={(map) => (this.mapNode = map)}></div>);
     };
 };
 
