89a1317c141abc7fe45daa54f18a4aafa974f57f
Nick_Cheung <nkkcheung@gmail.com>
2020-05-21 15:21:00 -0700

    404 q

--- frontend/components/no_content.jsx
+++ frontend/components/no_content.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import { browserHistory } from 'react-router';
 
 
 class NoContent extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+
+    }
 
     render() {
         return (
             <div className="nonContent">
                 <h1>404</h1>
                 <h2>Whoops! This is a little embarassing. Looks like we got lost on the trail.</h2>
-                <div>Take me back!</div>
+                <div onClick={this.handleClick}>Take me back!</div>
             </div>
         )
     }
