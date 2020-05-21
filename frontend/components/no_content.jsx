d8d7ec0c38c7253e51d67cc341028c1d4878c92d
Nick_Cheung <nkkcheung@gmail.com>
2020-05-21 16:58:15 -0700

    noncomponent

--- frontend/components/no_content.jsx
+++ frontend/components/no_content.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import { browserHistory } from 'react-router';
 
 
 class NoContent extends React.Component {
     constructor(props) {
         super(props)
 
-        this.handleClick = this.handleClick.bind(this)
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick() {
-
+        this.props.history.goBack();
     }
 
     render() {
@@ -19,7 +18,7 @@ class NoContent extends React.Component {
             <div className="nonContent">
                 <h1>404</h1>
                 <h2>Whoops! This is a little embarassing. Looks like we got lost on the trail.</h2>
-                <div onClick={this.handleClick}>Take me back!</div>
+                <button className="goBack" onClick={this.handleClick}>Take me back!</button>
             </div>
         )
     }
