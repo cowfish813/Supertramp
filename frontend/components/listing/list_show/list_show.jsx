7e4d0f1f1cea6579ce5feea8bdf82a1cfe76c504
Nick_Cheung <nkkcheung@gmail.com>
2020-06-03 15:42:45 -0700

    booking form

--- frontend/components/listing/list_show/list_show.jsx
+++ frontend/components/listing/list_show/list_show.jsx
@@ -36,135 +36,136 @@ class ListShow extends React.Component {
               <div className="photo_title">{this.props.list.name}</div>
             </div>
 
-              {/* <div className="absolute_booking"> */}
-            <div className="booking-comp">
-              <BookingForm
-                list={this.props.list}
-                listing_name={this.props.list.name}
-                host_id={this.props.list.host_id}
-                fetchListing={this.props.fetchListing}
-              />
-            </div>
-            {/* </div> */}
-            
-            <div className="show-listing">
-              <div className="section-overview">
 
-                <div className="list_overview">
-                  <img className="hostpic" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/24-248366_profile-clipart-generic-user-generic-profile-picture-gender.png" alt="" />
-                  <div className="host_by">
-                    Hosted By:
-                    <div className="host_name">Nick C.</div>
-                  </div>
+            <div className="booking_limit">
 
-                  <div className="details-container">
-                    <p className="description"> {this.props.list.description}</p>
-                  </div>
-                </div>
-
-                <div className="area_essentials_ammenities">
-                  <div className="camp_area_list condition_list">
-                    <div className="aea_title">Camp Area</div>
-                    {/* <div className="aea_info">
-                      <img className="aea_conditions" src="" alt=""/>
-                    </div> */}
-                    <div className="aea_info">
-                      <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/tent2.png" alt="" />
-                      <p>Bring your own Tent</p>
-                    </div>
-                    <div className="aea_info">
-                      <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/parking1-512.png" alt="" />
-                      <p>Parking at or near listing</p>
-                    </div>
-                  </div>
-                  <div className="essentials_list condition_list">
-                    <div className="aea_title">Essentials</div>                  
-                    <div className="aea_info">
-                      <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/toilet.png" alt="" />
-                      <p>Available</p>
-                    </div>
-                    <div className="aea_info">
-                      <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/bonfire.png" alt="" />
-                      <p>Allowed</p>
-                    </div>
-                  </div>
-                  <div className="amenities_list condition_list">
-                    <div className="aea_title">Ammenities</div>
-                    <div className="aea_info">
-                      <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/water.png" alt="" />
-                      <p>Potable Water</p>
-                    </div>
-                    <div className="aea_info">
-                      <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/5593_-_No_Wifi-512.png" alt="" />
-                      <p>No WiFi</p>
-                    </div>
-                    <div className="aea_info">
-                      <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/nowater.png" alt="" />
-                      <p>No Showers</p>
-                    </div>
-                    <div className="aea_info">
-                      <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/pacout.png" alt="" />
-                      <p>Pack out Waste</p>
-                    </div>
-
-
-                    <img className="aea_conditions" src="" alt="" />
-                  </div>
-
-                </div>
+              <div className="booking-comp">
+                <BookingForm
+                  list={this.props.list}
+                  listing_name={this.props.list.name}
+                  host_id={this.props.list.host_id}
+                  fetchListing={this.props.fetchListing}
+                />
               </div>
-              <div className="detail_list" id="details_sum_list">
-                <div className="details_text" className="detail_sum_list">Details</div>
-                <div className="details_text">
-                  <div>
-                    <strong>Check in: </strong> After 2PM{" "}
+              <div className="show-listing">
+                <div className="section-overview">
+
+                  <div className="list_overview">
+                    <img className="hostpic" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/24-248366_profile-clipart-generic-user-generic-profile-picture-gender.png" alt="" />
+                    <div className="host_by">
+                      Hosted By:
+                      <div className="host_name">Nick C.</div>
+                    </div>
+
+                    <div className="details-container">
+                      <p className="description"> {this.props.list.description}</p>
+                    </div>
                   </div>
-                  <div>
-                    <strong>Check out: </strong> Before 12PM
+
+                  <div className="area_essentials_ammenities">
+                    <div className="camp_area_list condition_list">
+                      <div className="aea_title">Camp Area</div>
+                      {/* <div className="aea_info">
+                        <img className="aea_conditions" src="" alt=""/>
+                      </div> */}
+                      <div className="aea_info">
+                        <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/tent2.png" alt="" />
+                        <p>Bring your own Tent</p>
+                      </div>
+                      <div className="aea_info">
+                        <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/parking1-512.png" alt="" />
+                        <p>Parking at or near listing</p>
+                      </div>
+                    </div>
+                    <div className="essentials_list condition_list">
+                      <div className="aea_title">Essentials</div>                  
+                      <div className="aea_info">
+                        <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/toilet.png" alt="" />
+                        <p>Available</p>
+                      </div>
+                      <div className="aea_info">
+                        <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/bonfire.png" alt="" />
+                        <p>Allowed</p>
+                      </div>
+                    </div>
+                    <div className="amenities_list condition_list">
+                      <div className="aea_title">Ammenities</div>
+                      <div className="aea_info">
+                        <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/water.png" alt="" />
+                        <p>Potable Water</p>
+                      </div>
+                      <div className="aea_info">
+                        <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/5593_-_No_Wifi-512.png" alt="" />
+                        <p>No WiFi</p>
+                      </div>
+                      <div className="aea_info">
+                        <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/nowater.png" alt="" />
+                        <p>No Showers</p>
+                      </div>
+                      <div className="aea_info">
+                        <img className="aea_conditions" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/pacout.png" alt="" />
+                        <p>Pack out Waste</p>
+                      </div>
+
+
+                      <img className="aea_conditions" src="" alt="" />
+                    </div>
+
                   </div>
-                  <div>
-                    <strong>Cancellation policy: </strong>{" "}
-                    {this.props.list.cancellation_policy}
+                </div>
+                <div className="detail_list" id="details_sum_list">
+                  <div className="details_text" className="detail_sum_list">Details</div>
+                  <div className="details_text">
+                    <div>
+                      <strong>Check in: </strong> After 2PM{" "}
+                    </div>
+                    <div>
+                      <strong>Check out: </strong> Before 12PM
+                    </div>
+                    <div>
+                      <strong>Cancellation policy: </strong>{" "}
+                      {this.props.list.cancellation_policy}
+                    </div>
+                  </div>
+
+                  <div className="details_text">
+                    <div>
+                      <strong>On Arrival: </strong> {this.props.list.on_arrival}{" "}
+                    </div>
+                    <div>
+                      <strong>Minimum nights: </strong> 1 night
+                    </div>
+                    <div>
+                      <strong> Weekend discount: </strong> None{" "}
+                    </div>
                   </div>
                 </div>
 
-                <div className="details_text">
-                  <div>
-                    <strong>On Arrival: </strong> {this.props.list.on_arrival}{" "}
-                  </div>
-                  <div>
-                    <strong>Minimum nights: </strong> 1 night
-                  </div>
-                  <div>
-                    <strong> Weekend discount: </strong> None{" "}
+                <div className="detail_list" id="list-offset">
+                  <div className="details_title">Terrain</div>
+                  <div className="img_Detail">
+                    <img
+                      className="detail_img"
+                      src="https://supertramp-mast.s3-us-west-1.amazonaws.com/Screen+Shot+2020-05-08+at+1.05.38+AM.png"
+                      alt=""
+                    />
                   </div>
                 </div>
+
+                <div className="detail_list">
+                  <div className="details_title" id="img_offset">Activities</div>
+
+                  <div className="img_Detail">
+                    <img
+                      className="detail_img"
+                      src="https://supertramp-mast.s3-us-west-1.amazonaws.com/Screen+Shot+2020-05-08+at+1.03.40+AM.png"
+                      alt=""
+                    />
+                  </div>
+                </div>
+
+                <div className="reviews_container">{/* review index item */}</div>
               </div>
-
-              <div className="detail_list" id="list-offset">
-                <div className="details_title">Terrain</div>
-                <div className="img_Detail">
-                  <img
-                    className="detail_img"
-                    src="https://supertramp-mast.s3-us-west-1.amazonaws.com/Screen+Shot+2020-05-08+at+1.05.38+AM.png"
-                    alt=""
-                  />
-                </div>
-              </div>
-
-              <div className="detail_list">
-                <div className="details_title" id="img_offset">Activities</div>
-
-                <div className="img_Detail">
-                  <img
-                    className="detail_img"
-                    src="https://supertramp-mast.s3-us-west-1.amazonaws.com/Screen+Shot+2020-05-08+at+1.03.40+AM.png"
-                    alt=""
-                  />
-                </div>
-              </div>
-
-              <div className="reviews_container">{/* review index item */}</div>
             </div>
             <ListMap
               className="map"
