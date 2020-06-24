b807bb0ad2187ae4e8389c79c4fb6345e9e7095b
Nick_Cheung <nkkcheung@gmail.com>
2020-06-24 15:43:57 -0700

    react hooks no content

--- frontend/components/no_content.jsx
+++ frontend/components/no_content.jsx
@@ -1,33 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import { withRouter } from "react-router-dom";
 
+const NoContent = (props) => {
+    const [state, setState] = useState({
 
-class NoContent extends React.Component {
-    constructor(props) {
-        super(props)
+    })
 
-        this.handleClick = this.handleClick.bind(this);
-    }
+    return (
+        <div className="nonContent">
+            <h1>404</h1>
+            <h2>Whoops! This is a little embarassing.</h2>
+            <h2>Looks like we got lost on the trail.</h2>
+            <img className="noContentPic" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/70508342_403917306911081_545636708322902016_n.gif" alt="" />
+            {/* <button className="goBack" onClick={this.handleClick}>Take me back!</button> */}
+        </div>
+    )
+};
 
-    componentDidMount() {
-        window.scrollTo(0, 0);
-    }
+export default NoContent
 
-    handleClick() {
-        this.props.history.goBack();
-    }
+// class NoContent extends React.Component {
+//     constructor(props) {
+//         super(props)
 
-    render() {
-        return (
-            <div className="nonContent">
-                <h1>404</h1>
-                <h2>Whoops! This is a little embarassing.</h2> 
-                <h2>Looks like we got lost on the trail.</h2>
-                <img className="noContentPic" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/70508342_403917306911081_545636708322902016_n.gif" alt=""/>
-                <button className="goBack" onClick={this.handleClick}>Take me back!</button>
-            </div>
-        )
-    }
-}
+//         this.handleClick = this.handleClick.bind(this);
+//     }
 
-export default withRouter(NoContent)
+//     componentDidMount() {
+//         window.scrollTo(0, 0);
+//     }
+
+//     handleClick() {
+//         this.props.history.goBack();
+//     }
+
+//     render() {
+//         return (
+//             <div className="nonContent">
+//                 <h1>404</h1>
+//                 <h2>Whoops! This is a little embarassing.</h2> 
+//                 <h2>Looks like we got lost on the trail.</h2>
+//                 <img className="noContentPic" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/70508342_403917306911081_545636708322902016_n.gif" alt=""/>
+//                 <button className="goBack" onClick={this.handleClick}>Take me back!</button>
+//             </div>
+//         )
+//     }
+// }
+
+// export default withRouter(NoContent)
+
