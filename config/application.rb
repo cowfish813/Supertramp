2ab9e0a1573149e046f0b1dfdbcbc7a24a1430fc
Nicholas Cheung <ncheung@carboncopies.org>
2023-10-04 11:53:47 -0700

    webpacker

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Supertrampapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
       #reflects new rails from 5-6
         #6 to 6.1
 
