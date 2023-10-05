2c00791fea8358e171ba128f015e1772da14d7ee
Nicholas Cheung <ncheung@carboncopies.org>
2023-10-05 12:15:47 -0700

    commented code?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { :host => 'https://supertrampapp-heroku-22-30a11ea3b26f.herokuapp.com' }
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
