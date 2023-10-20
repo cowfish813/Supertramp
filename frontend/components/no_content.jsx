2464ab5a9dba4b7f2cc1da9d14e09e26ddb39731
Nicholas Cheung <ncheung@carboncopies.org>
2023-10-20 15:27:50 -0700

    commented code

--- frontend/components/no_content.jsx
+++ frontend/components/no_content.jsx
@@ -19,36 +19,3 @@ function handleClick(props) {
 };
 
 export default withRouter(NoContent);
-
-//probably not needed anymore, there appears to be no issue.
-//maybe locate the actual image instead of what i have now?
-
-// class NoContent extends React.Component {
-//     constructor(props) {
-//         super(props)
-
-//         this.handleClick = this.handleClick.bind(this);
-//     }
-
-//     componentDidMount() {
-//         window.scrollTo(0, 0);
-//     }
-
-    // handleClick() {
-    //     this.props.history.goBack();
-    // }
-
-//     render() {
-//         return (
-//             <div className="nonContent">
-//                 <h1>404</h1>
-//                 <h2>Whoops! This is a little embarassing.</h2> 
-//                 <h2>Looks like we got lost on the trail.</h2>
-//                 <img className="noContentPic" src="https://supertramp-mast.s3-us-west-1.amazonaws.com/70508342_403917306911081_545636708322902016_n.gif" alt=""/>
-//                 <button className="goBack" onClick={this.handleClick}>Take me back!</button>
-//             </div>
-//         )
-//     }
-// }
-
-
