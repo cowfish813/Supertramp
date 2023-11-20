9a8ee863cf486dbd75582171a61233575e8487b0
Nicholas Cheung <ncheung@carboncopies.org>
2023-11-20 14:23:25 -0800

    container comments

--- frontend/components/search/search_container.jsx
+++ frontend/components/search/search_container.jsx
@@ -3,10 +3,6 @@ import Search from './search';
 import { withRouter } from 'react-router-dom';
 import { receiveLocation } from '../../actions/map_action';
 
-// const mSTP = (state, ownProps) => ({
-//     listings: Object.values(state.entities.listings)
-// })
-
 const mDTP = dispatch => ({
     receiveLocation: (location) => dispatch(receiveLocation(location))
 });
