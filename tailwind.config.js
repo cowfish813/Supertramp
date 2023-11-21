b58168539869ad5676fb02e9e0e1a21100541cae
Nicholas Cheung <ncheung@carboncopies.org>
2023-11-21 11:16:04 -0800

    revert

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./frontend/components/**/*.{js,jsx,ts,tsx,html}",
+    "./node_modules/react-dates/**/*"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
