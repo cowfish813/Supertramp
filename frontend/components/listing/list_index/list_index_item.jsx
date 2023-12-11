a265318ab00460b2613ba13726f0efc8f847acaf
Nicholas Cheung <ncheung@carboncopies.org>
2023-12-11 15:29:08 -0800

    index item

--- frontend/components/listing/list_index/list_index_item.jsx
+++ frontend/components/listing/list_index/list_index_item.jsx
@@ -17,21 +17,22 @@ class IndexItem extends React.Component {
     render() {
         return ( 
             <div className="index_container">
+
                 <div className="index_box">
                     <Link to={`/listings/${this.props.listing.id}`}>
                         <img className="indexPhotos" src={this.props.listing.photoUrls} alt=""/>
                     </Link>
                 </div>
-                <div>
-                    <div className="listing_index_desc">
-                        <div className="list_index_name">{this.props.listing.name}</div>
-                        <div className="listing_index_price margin_left2">From 
-                            <div className="bold">
-                                ${this.props.listing.price}
-                            </div>/day
-                        </div>
+
+                <div className="listing_index_desc">
+                    <div className="list_index_name">{this.props.listing.name}</div>
+                    <div className="listing_index_price margin_left2">From 
+                        <div className="bold">
+                            ${this.props.listing.price}
+                        </div>/day
                     </div>
                 </div>
+                
             </div>
         )
     };
