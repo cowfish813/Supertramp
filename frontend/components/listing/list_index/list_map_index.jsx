cac154a6ac0bfb2386cf3e2acb3e62b475194c2a
Nicholas Cheung <ncheung@carboncopies.org>
2024-02-20 13:56:56 -0800

    removed commented functional component

--- frontend/components/listing/list_index/list_map_index.jsx
+++ frontend/components/listing/list_index/list_map_index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import MarkerManager from '../../../util/map_marker';
 import { withRouter } from 'react-router-dom';
 import ReactDOMServer from "react-dom";
-// import { useState } from 'react';
 
 class ListMapIndex extends React.Component {
     constructor(props) {
@@ -40,7 +39,6 @@ class ListMapIndex extends React.Component {
             zoom: 13,
             mapTypeId: "terrain",
         };
-    //this.map = undefined D:
 
         this.map = new google.maps.Map(this.mapNode, this.mapOptions);
         this.MarkerManager = new MarkerManager(this.map, this.handleMarkerClick.bind(this));
@@ -102,83 +100,5 @@ class ListMapIndex extends React.Component {
         )
     };
 }
-
-// const ListMapIndex =(props) => {
-//     const [state, setState] = useState(() => {
-//         if (props.mapLocation.state) {
-//             return {
-//                 lat: props.mapLocation.state.lat,
-//                 lng: props.mapLocation.state.lng,
-//             };
-//         } else {
-//             return props.mapLocation;
-//         }
-//     });
-//     const mapOptions = useRef({
-//         center: {
-//             lat: state.lat,
-//             lng: state.lng
-//         },
-//         zoom: 13,
-//         mapTypeId: "terrain",
-//     });
-//     const mapNode = useRef(null);
-//     const map = useRef(null);
-//     const MarkerManager = useRef(null);
-
-//     useEffect(() => {
-//         window.scrollTo(0, 0);
-//         mapOptions.current = {
-//             center: {
-//                 lat: state.lat,
-//                 lng: state.lng
-//             },
-//             zoom: 13,
-//             mapTypeId: "terrain",
-//         };
-//         map.current = new google.maps.Map(mapNode.current, mapOptions.current);
-//         MarkerManager.current = new MarkerManager(map.current, handleMarkerClick);
-//         registerListeners();
-//         MarkerManager.current.updateMarkers(props.listings);
-//     }, []);
-
-//     useEffect(() => {
-//         if (props.mapLocation.lat !== state.lat || props.mapLocation.lng !== state.lng) {
-//             setState({
-//                 lat: props.mapLocation.lat,
-//                 lng: props.mapLocation.lng,
-//             });
-//         }
-//     }, [props.mapLocation]);
-
-//     useEffect(() => {
-//         if (props.listings !== MarkerManager.current.props.listings) {
-//             MarkerManager.current.updateMarkers(props.listings);
-//         }
-//     }, [props.listings]);
-
-//     const registerListeners = () => {
-//         google.maps.event.addListener(map.current, 'idle', () => {
-//             const { north, south, east, west } = map.current.getBounds().toJSON();
-//             const bounds = {
-//                 northEast: { lat: north, lng: east },
-//                 southWest: { lat: south, lng: west }
-//             };
-//             props.updateFilter(bounds);
-//         });
-//     };
-
-//     const handleMarkerClick = (listing) => {
-//         props.history.push(`/listings/${listing.id}`);
-//     };
-
-//     return (
-//         <div
-//             className="mapIndex"
-//             id="mapNode"
-//             ref={mapNode}
-//         />
-//     );
-// }
 
 export default withRouter(ListMapIndex);
