5af383acc29ea11e247187bdd515c67fd5a5c5d6
Nicholas Cheung <ncheung@carboncopies.org>
2024-07-25 16:58:05 -0700

    bookinog css border

--- frontend/components/bookings/booking_form.jsx
+++ frontend/components/bookings/booking_form.jsx
@@ -103,19 +103,23 @@ class BookingForm extends React.Component {
       
         return (
           <div className="widget-container">
-            <form className="wrapper" onSubmit={this.handleSubmit}>
-              <div className="price-wrapper">
+            <form className="w100" onSubmit={this.handleSubmit}>
+              <div className="price-wrapper  booking-border-bot">
                 <div className="price">
                   {`$${this.props.list.price}`}
                   <p>per night</p>
                   <span className="booking_errors">{BookingError}</span>
                 </div>
               </div>
-              <div className="dates-and-guest-content">
-                <div className="col checkin" >
-                  <div className="label " >
+
+              <div className="dates-and-guest-content booking-border-bot">
+                <div className="col " >
+                  {/* <div className="label booking-label" > */}
                     <p className="flex-col-center">Add dates</p>
-                      <DatePicker 
+                        
+                  {/* </div> */}
+                      <DatePicker
+                          // wrapperClassName="datePicker"
                           showIcon
                           selected={this.state.check_in} 
                           onChange={this.handleCheckIn}
@@ -126,15 +130,15 @@ class BookingForm extends React.Component {
                           monthsShown={2}
                         /> 
                         
-                  </div>
                 </div>
 
-                <div className="col capacity">
+                {/* <div className="col capacity"> */}
                   <div className="label">
-                    Add guests
-                    <div className="SingleDatePicker SingleDatePicker_1">
-                      <div className="SingleDatePickerInput SingleDatePickerInput_1">
-                        <div className="DateInput DateInput_1">
+                    <p>Add guests
+</p>
+                    {/* <div className="SingleDatePicker SingleDatePicker_1">
+                      <div className="SingleDatePickerInput SingleDatePickerInput_1"> */}
+                        {/* <div className=""> */}
                           <input
                             type="number"
                             name="capacity"
@@ -145,12 +149,14 @@ class BookingForm extends React.Component {
                             max={this.props.list.capacity}
                             onChange={() => this.handleCapacity}
                           />
-                        </div>
-                      </div>
-                    </div>
+                        {/* </div> */}
+                      {/* </div>
+                    </div> */}
                   </div>
-                </div>
+                {/* </div> */}
+
               </div>
+              
               <button className="booking-button">Request to Book</button>
             </form>
           </div>
