8ced03d05ae459406264b9881e6d9889186c8d13
Nicholas Cheung <ncheung@carboncopies.org>
2024-08-02 16:49:11 -0700

    booking slice of state for exclusions

--- frontend/components/bookings/booking_form_container.js
+++ frontend/components/bookings/booking_form_container.js
@@ -1,19 +1,22 @@
 import BookingForm from './booking_form';
 import { connect } from 'react-redux';
 import { openModal } from '../../actions/modal_actions/modal_actions';
-import { createBooking, receiveBookingErrors } from '../../actions/booking_actions/booking_action';
+import { createBooking, receiveBookingErrors, fetchBookings } from '../../actions/booking_actions/booking_action';
 
 const mstp = (state, ownProps) => {
     return ({
       currentUser: state.entities.users[state.session.currentUser],
-      errors: state.errors.bookings
+      errors: state.errors.bookings,
+      bookings: Object.values(state.entities.bookings)
     });
 };
 
 const mdtp = dispatch => ({
   openModal: modal => dispatch(openModal(modal)),
   createBooking: booking => dispatch(createBooking(booking)),
-  removeBookingErrors: (empty) => dispatch(receiveBookingErrors(empty))
+  removeBookingErrors: (empty) => dispatch(receiveBookingErrors(empty)),
+  requestUser: (user) => dispatch(requestUser(user)),
+  fetchBookings: (userId) => dispatch(fetchBookings(userId))
 });
 
 export default connect(mstp, mdtp)(BookingForm);
