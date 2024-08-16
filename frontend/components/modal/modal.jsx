73666542c2e7c184ebabe494081af10b4baee20b
Nicholas Cheung <ncheung@carboncopies.org>
2024-08-16 13:33:28 -0700

    functional conversion

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
-import {closeModal} from '../../actions/modal_actions/modal_actions'
-import {connect} from 'react-redux'
-import LoginFormContainer from '../session/login_form_container'
-import SignupFormContainer from '../session/signup_form_container'
-import BookingUpdateForm from '../bookings/booking_update_form_container'
+import React, {useState, useEffect} from 'react';
+import {closeModal} from '../../actions/modal_actions/modal_actions';
+import LoginFormContainer from '../session/login_form_container';
+import SignupFormContainer from '../session/signup_form_container';
+import BookingUpdateForm from '../bookings/booking_update_form_container';
+import { useSelector, useDispatch } from 'react-redux';
+// import {connect} from 'react-redux'
 
-function Modal({ errors, modal, closeModal }) {
+function Modal() {
+    const dispatch = useDispatch();
+    const modal = useSelector(state => state.ui.modal);
+    const errors = useSelector(state => state.errors.session);
+    const [errorModal, setErrorModal] = useState(<div id="error-empty"></div>);
+
+    const handleCloseModal = () => {
+        dispatch(closeModal());
+    };
+
     if (!modal) return null
 
-    let errorModal = <div id="error-empty"></div>
-    if (errors.length > 0) {
-        errorModal = (<div id="error-content" className="feedback-indicator">
-            {errors.map((error, i) => {
-                return  <li key={i}>{error}</li>
-            })}
-        </div>)
-    }
+    useEffect(() => {
+        if (errors.length > 0) {
+            setErrorModal(<div id="error-content" className="feedback-indicator">
+                {errors.map((error, i) => {
+                    return  <li key={i}>{error}</li>
+                })}
+            </div>)
+            // const parent = document.getElementById('error-parent')
+        } else {
+            setErrorModal(<div id="error-empty"></div>)
+        }
+    }, [errors])
+    
 
     let component
     switch (modal.modal) { 
@@ -35,37 +50,19 @@ function Modal({ errors, modal, closeModal }) {
         default:
             return null
     };
-    //currently passing in a string that opens up forms
-    //find a way to pass in listing ID
-    //
-
-
 
     return (
         <div>
-            <div className="modal-background" onClick={ closeModal }>
+            <div className="modal-background" onClick={ handleCloseModal }>
                 <div className="modal-child" onClick={e => e.stopPropagation()}>
                     { component } 
                 </div>
                 <div id="error-parent">
-                { errorModal }
+                    { errorModal }
                 </div>
             </div>
         </div>
     );
 }
 
-
-
-
-const mSTP = state => ({
-    modal: state.ui.modal,
-    errors: state.errors.session
-})
-
-const mDTP = dispatch => ({
-    closeModal: () => dispatch(closeModal()),
-
-})
-
-export default connect(mSTP, mDTP)(Modal)
+export default Modal
