eacfc71e80338bb2b70983a0d1ab348ac5a5f3e3
Nicholas Cheung <ncheung@carboncopies.org>
2025-05-07 16:17:24 -0700

    boot

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
-
+ENV["EXECJS_RUNTIME"] = "Node"
+ENV["PATH"] = "/opt/homebrew/bin:#{ENV["PATH"]}"  # Ensure Homebrew Node is in PATH
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
