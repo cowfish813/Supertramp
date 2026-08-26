08146dd353f0efddb8aeef004482eb4e7008f51f
Nicholas Cheung <ncheung@carboncopies.org>
2026-08-26 14:34:55 -0700

    exported icons to different component files

---
+++ frontend/components/icons/LocationDot.jsx
@@ -0,0 +1,8 @@
+const LocationDot = ({ className }) => (
+    <svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12 21s7-7.5 7-12a7 7 0 10-14 0c0 4.5 7 12 7 12z" stroke="black" strokeWidth="2"/>
+        <circle cx="12" cy="9" r="2.5" stroke="black" strokeWidth="2"/>
+    </svg>
+);
+
+export default LocationDot;
---
+++ frontend/components/icons/SearchIcon.jsx
@@ -0,0 +1,7 @@
+const SearchIcon = ({ className }) => (
+    <svg className={className} viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="10" cy="10" r="9" stroke="white" strokeWidth="3"/>
+        <line x1="16.5" y1="16.5" x2="25" y2="25" stroke="white" strokeWidth="3.25" fill='currentColor' strokeLinecap="round"/>
+    </svg>);
+
+export default SearchIcon;
