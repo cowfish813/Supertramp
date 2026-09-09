724e80f2a03c976572c365a8a3c8e6e7801c9ef2
Nicholas Cheung <ncheung@carboncopies.org>
2026-09-09 14:00:27 -0700

    webp conversion

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -23,6 +23,8 @@
 #
 #  index_listings_on_host_id  (host_id)
 #
+require 'image_processing/vips'
+
 class Listing < ApplicationRecord
     validates :cancellation_policy, :capacity, :description, :minimum_nights, :name, :on_arrival, :price, presence: true
 
@@ -35,6 +37,7 @@ class Listing < ApplicationRecord
     dependent: :destroy
 
     has_many_attached :photos
+    after_commit :convert_photos_to_webp, on: [:create, :update]
 
     # def ensure_photo
     #     unless self.photo.attached?
@@ -75,6 +78,44 @@ class Listing < ApplicationRecord
     foreign_key: :listing_id,
     dependent: :destroy
 
-    
+    private
+
+    def convert_photos_to_webp
+        return if Rails.env.test? || ENV['SKIP_WEBP_CONVERSION']
+        photos_to_convert = photos.select{ |p| p.content_type.in?(%w[image/jpeg])}
+        return if photos_to_convert.empty?
+
+        photos_to_convert.each do |photo|
+            begin
+                convert_single_photo(photo)
+            rescue => exception
+                # Rails.logger.error("WebP conversion failed for photo #{photo.id}": "#{exception.message}")
+                Rails.logger.error("WebP conversion failed for photo #{photo.id}: #{exception.message}")
+                next
+            end
+
+        end
+    end
+
+    def convert_single_photo(photo)
+        converted_io = ImageProcessing::Vips
+            .source(photo.download)
+            .convert('webp')
+            .saver(quality: 85)
+            .call
+
+        new_filename="#{File.(photo.filename.to_s, '.*')}.webp"
+
+        new_blob = ActiveStorage::Blob.create_and_upload!(
+            io: File.open(converted_io.path),
+            filename: new_filename,
+            content_type: 'image/webp'
+        )
+
+        photos.attach(new_blob)
+        photo.purge#_later
+    ensure
+        converted_io&.close! #if converted_io.respond_to?(:close!)
+    end
 
 end
