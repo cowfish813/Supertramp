caa86745faa2ddf54d669247685e3b053a50633a
Nicholas Cheung <ncheung@carboncopies.org>
2026-09-15 15:49:11 -0700

    removed redux and added redux toolkit. removed mentions of redux on imports

--- frontend/supertramp.jsx
+++ frontend/supertramp.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from './components/root'
 import { createRoot } from 'react-dom/client'; // Import createRoot
-import {fetchListings} from './actions/listing_actions/listing_actions'
+// import {fetchListings} from './actions/listing_actions/listing_actions'
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
